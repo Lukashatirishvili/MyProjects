1682da5ada5656b128315ce282c2d7261f7d50d1
Luka Shatirishvili <lukkashatirishvili@gmail.com>
2023-10-13 22:04:00 +0400

    update

--- todolist/script.js
+++ todolist/script.js
@@ -2,7 +2,11 @@
 const input = document.querySelector(".input");
 const taskSection = document.querySelector(".task-section");
 const filterSection = document.querySelector(".filter-section");
+const filterButton = document.querySelector(".filter-buttons");
 const arrowBtn = document.querySelector(".arrow-img");
+const allBtn = document.querySelector(".all-button");
+const activeBtn = document.querySelector(".active-button");
+const completedBtn = document.querySelector(".completed-button");
 
 // ******** Functions ******** //
 function createTask(e) {
@@ -10,7 +14,7 @@ function createTask(e) {
     // Create and append "task" element
     const task = document.createElement("div");
     task.classList.add("task", "active");
-    task.innerHTML = `<img src="images/unchecked-btn.png" class="check-img" />
+    task.innerHTML = `<img src="images/unchecked-btn.png" class="check-img unchecked" />
     <input readonly type="text" class="task-field" placeholder='${input.value}' />
     <img src="images/remove-btn.png" class="remove-img hidden" />`;
     taskSection.appendChild(task);
@@ -22,55 +26,153 @@ function createTask(e) {
 
     // Display item
     itemCouner();
+
+    // Display arrow btn
+    displayArrowBtn();
+  }
+}
+
+function filterBtnLogic(e) {
+  const target = e.target;
+
+  const allCompletedTask = document.querySelectorAll(".task.completed");
+  const allActiveTask = document.querySelectorAll(".task.active");
+  if (target.matches(".active-button")) {
+    allBtn.classList.remove("active");
+    completedBtn.classList.remove("active");
+    activeBtn.classList.add("active");
+    allActiveTask.forEach((task) => {
+      task.classList.remove("hidden");
+    });
+    allCompletedTask.forEach((task) => {
+      task.classList.add("hidden");
+    });
+  } else if (target.matches(".completed-button")) {
+    activeBtn.classList.remove("active");
+    allBtn.classList.remove("active");
+    completedBtn.classList.add("active");
+    allActiveTask.forEach((task) => {
+      task.classList.add("hidden");
+    });
+    allCompletedTask.forEach((task) => {
+      task.classList.remove("hidden");
+    });
+  } else if (target.matches(".all-button")) {
+    activeBtn.classList.remove("active");
+    completedBtn.classList.remove("active");
+    allBtn.classList.add("active");
+    allActiveTask.forEach((task) => {
+      task.classList.remove("hidden");
+    });
+    allCompletedTask.forEach((task) => {
+      task.classList.remove("hidden");
+    });
   }
 }
 
 function completeTask(e) {
   const target = e.target; // element which will be clicked
-  const task = target.parentElement;
-
-  if (!target.matches(".checked")) {
-    // mark task as completed
-    task.classList.remove("active");
-    task.classList.add("completed");
-
-    // mark check button and update item
-    checkBtn(target);
-    itemCouner();
-  } else {
-    // mark task as uncompleted
-    task.classList.remove("completed");
-    task.classList.add("active");
-
-    // mark check button and update item
-    checkBtn(target);
-    itemCouner();
-  }
+  const taskEl = document.querySelectorAll(".task");
+  checkBtn(target);
+  // display arrow btn
+  displayArrowBtn();
+  displayClearBtn();
+  itemCouner();
 }
 
 function checkBtn(target) {
   const taskEl = document.querySelectorAll(".task");
-  if (!target.matches(".check-img.checked")) {
+  if (target.matches(".unchecked")) {
+    // modify check button
+    target.classList.remove("unchecked");
+    target.classList.add("checked");
+    target.src = "images/checked-btn.png";
+    // modify task element
+    target.parentElement.classList.remove("active");
+    target.parentElement.classList.add("completed");
+    if (activeBtn.matches(".active")) {
+      target.parentElement.classList.add("hidden");
+    }
+    // display clearbtn
+  } else if (target.matches(".checked")) {
+    // modify check button
+    target.classList.remove("checked");
+    target.classList.add("unchecked");
+    target.src = "images/unchecked-btn.png";
+    // modify task element
+    target.parentElement.classList.remove("completed");
+    target.parentElement.classList.add("active");
+    if (completedBtn.matches(".active")) {
+      target.parentElement.classList.add("hidden");
+    }
+  }
+  // update item counter
+  itemCouner();
+}
+
+function arrowBtn_logic(target) {
+  const taskEl = document.querySelectorAll(".task");
+  if (target.matches(".unclicked")) {
+    arrowBtn.src = "images/arrow-clicked.png";
+    arrowBtn.classList.remove("unclicked");
+    arrowBtn.classList.add("clicked");
     taskEl.forEach((task) => {
-      const checkBtn = task.children[0];
-      if (task.matches(".completed")) {
-        checkBtn.src = "images/checked-btn.png";
-        checkBtn.classList.add("checked");
-      } else if (task.matches(".active")) {
-        checkBtn.src = "images/unchecked-btn.png";
-        checkBtn.classList.remove("checked");
-      }
-    });
-  } else if (target.matches(".arrow-img")) {
-    taskEl.forEach((task) => {
-      const checkBtn = task.children[0];
-      // change img src and classlist
-      checkBtn.src = "images/checked-btn.png";
-      checkBtn.classList.add("checked");
-      // change task classlist
       task.classList.remove("active");
       task.classList.add("completed");
+      task.children[0].classList.remove("unchecked");
+      task.children[0].classList.add("checked");
+      task.children[0].src = "images/checked-btn.png";
+      if (activeBtn.matches(".active")) {
+        task.classList.add("hidden");
+      }
+      itemCouner();
+      displayClearBtn();
     });
+  } else if (target.matches(".clicked")) {
+    arrowBtn.src = "images/arrow.png";
+    arrowBtn.classList.remove("clicked");
+    arrowBtn.classList.add("unclicked");
+    taskEl.forEach((task) => {
+      task.classList.remove("completed");
+      task.classList.add("active");
+      task.children[0].classList.remove("checked");
+      task.children[0].classList.add("unchecked");
+      task.children[0].src = "images/unchecked-btn.png";
+      if (completedBtn.matches(".active")) {
+        task.classList.add("hidden");
+      }
+      itemCouner();
+      displayClearBtn();
+    });
+  }
+}
+
+function displayClearBtn() {
+  const clearBtn = document.querySelector(".clear-task");
+  const taskActive_El = document.querySelectorAll(".task.active");
+  const taskCompleted_El = document.querySelectorAll(".task.completed");
+  if (taskCompleted_El.length > 0) {
+    clearBtn.classList.remove("hidden");
+  } else if (taskCompleted_El.length == 0) {
+    clearBtn.classList.add("hidden");
+  }
+}
+
+function displayArrowBtn() {
+  const taskEl = document.querySelectorAll(".task");
+
+  const allHaveCompletedClass = [...taskEl].every((element) =>
+    element.classList.contains("completed")
+  );
+
+  if (allHaveCompletedClass) {
+    arrowBtn.src = "images/arrow-clicked.png";
+    arrowBtn.classList.remove("unclicked");
+    arrowBtn.classList.add("clicked");
+  } else {
+    arrowBtn.src = "images/arrow.png";
+    arrowBtn.classList.remove("clicked");
+    arrowBtn.classList.add("unclicked");
   }
 }
 
@@ -85,8 +187,9 @@ taskSection.addEventListener("click", function (e) {
   completeTask(e);
 });
 arrowBtn.addEventListener("click", function (e) {
-  checkBtn(e.target);
+  arrowBtn_logic(e.target);
 });
+filterButton.addEventListener("click", filterBtnLogic);
 
 /*
 // ******** Selecting elements ******** //
