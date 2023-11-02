21091e69fd061f5387debc12d119f9f334c3bee7
Luka Shatirishvili <lukkashatirishvili@gmail.com>
2023-11-02 23:31:11 +0400

    update

--- todolist/script.js
+++ todolist/script.js
@@ -1,68 +1,60 @@
 // ******** Selecting Elements ********
+const todoApp = document.querySelector(".todo-app");
 const input = document.querySelector(".input");
 const taskSection_el = document.querySelector(".task-section");
-const arrowButton_el = document.querySelector(".arrow-btn");
-const filterSection_el = document.querySelector(".filter-section");
-const allBtn = document.querySelector(".all-button");
-const activeBtn = document.querySelector(".active-button");
-const completedBtn = document.querySelector(".completed-button");
+const allBtn = document.querySelector(".all-btn");
+const activeBtn = document.querySelector(".active-btn");
+const completedBtn = document.querySelector(".completed-btn");
+
+// ******** Event Listeners ********
+input.addEventListener("keydown", createTask);
+taskSection_el.addEventListener("mouseover", displayDeletebtn);
+taskSection_el.addEventListener("mouseout", displayDeletebtn);
+document.addEventListener("click", (e) => {
+  checkDelete_logic(e);
+  filterSection_logic(e);
+});
+todoApp.addEventListener("dblclick", updateTask);
 
 // ******** Functions ********
+getFromLocalStorage();
+
 function createTask(e) {
   if (e.keyCode === 13 && input.value) {
     const task_el = document.createElement("div");
     task_el.classList.add("task", "active");
-    task_el.innerHTML = `<img src="images/unchecked-btn.png" class="check-btn unchecked" />
-    <input type="text" class="task-field" readonly />
-    <img src="images/remove-btn.png" class="remove-btn hidden" />`;
-
+    task_el.innerHTML = `<button class="check unclicked">
+    <i class="fa-regular fa-circle-check fa-2xl"></i>
+  </button>
+  <input type="text" class="task-field" readonly />
+  <button class="delete">
+    <i class="fa-solid fa-xmark fa-lg"></i>
+  </button>`;
     taskSection_el.appendChild(task_el);
 
     const taskField_el = task_el.children[1];
     const checkBtn = task_el.children[0];
-    taskField_el.value = input.value;
+    taskField_el.placeholder = input.value;
+
+    if (completedBtn.matches(".clicked")) {
+      task_el.style.display = "none";
+    }
 
     input.value = "";
 
+    saveToLocalStorage();
     displayItem();
-
-    // when user click on check button
-    checkBtn.addEventListener("click", () => {
-      if (task_el.matches(".task.active")) {
-        task_el.classList.replace("active", "completed");
-        checkBtn.src = "images/checked-btn.png";
-        checkBtn.classList.replace("unchecked", "checked");
-        if (activeBtn.matches(".active")) {
-          task_el.classList.add("hidden");
-        }
-      } else if (task_el.matches(".task.completed")) {
-        task_el.classList.replace("completed", "active");
-        checkBtn.src = "images/unchecked-btn.png";
-        checkBtn.classList.replace("checked", "unchecked");
-        if (completedBtn.matches(".active")) {
-          task_el.classList.add("hidden");
-        }
-      }
-      displayItem();
-    });
-
-    // remove btn
-    const removeBtn = task_el.children[2];
-    task_el.addEventListener("mouseover", () => {
-      removeBtn.classList.remove("hidden");
-    });
-    task_el.addEventListener("mouseout", () => {
-      removeBtn.classList.add("hidden");
-    });
   }
 }
 
 function updateTask(e) {
   const target = e.target;
-  const checkBtn = target.previousElementSibling;
+  const task = target.parentElement;
   if (target.matches(".task-field")) {
     target.removeAttribute("readonly");
-    checkBtn.classList.add("hidden");
+    let temp = target.placeholder;
+    target.placeholder = "";
+    target.value = temp;
 
     target.addEventListener("keydown", (e) => {
       if (e.keyCode === 13) {
@@ -71,135 +63,183 @@ function updateTask(e) {
           displayItem();
         }
         target.setAttribute("readonly", "readonly");
-        checkBtn.classList.remove("hidden");
+        target.placeholder = target.value;
+        displayItem();
       }
+      saveToLocalStorage();
     });
 
     document.addEventListener("click", (e) => {
       if (!e.target.matches(".task-field")) {
         target.setAttribute("readonly", "readonly");
-        checkBtn.classList.remove("hidden");
         if (!target.value) {
           target.parentElement.remove();
           displayItem();
         }
+        target.placeholder = target.value;
+        displayItem();
       }
+      saveToLocalStorage();
     });
   }
 }
 
-function deleteTask(e) {
+function filterSection_logic(e) {
+  const task_el = document.querySelectorAll(".task");
   const target = e.target;
-  const taskCompleted_el = document.querySelectorAll(".task.completed");
-  if (target.matches(".remove-btn")) {
-    target.parentElement.remove();
+  if (target.matches(".all-btn")) {
+    task_el.forEach((task) => {
+      task.style.display = "flex";
+    });
+    allBtn.classList.add("clicked");
+    activeBtn.classList.remove("clicked");
+    completedBtn.classList.remove("clicked");
   }
 
-  if (target.matches(".clear-task")) {
+  if (target.matches(".active-btn")) {
+    task_el.forEach((task) => {
+      if (task.matches(".task.active")) {
+        task.style.display = "flex";
+      } else {
+        task.style.display = "none";
+      }
+      allBtn.classList.remove("clicked");
+      activeBtn.classList.add("clicked");
+      completedBtn.classList.remove("clicked");
+    });
+  }
+
+  if (target.matches(".completed-btn")) {
+    task_el.forEach((task) => {
+      if (task.matches(".task.completed")) {
+        task.style.display = "flex";
+      } else {
+        task.style.display = "none";
+      }
+      allBtn.classList.remove("clicked");
+      activeBtn.classList.remove("clicked");
+      completedBtn.classList.add("clicked");
+    });
+  }
+}
+
+function checkDelete_logic(e) {
+  const target = e.target;
+  const task_el = document.querySelectorAll(".task");
+  const taskCompleted_el = document.querySelectorAll(".task.completed");
+
+  const checkallBtn = document.querySelector(".checkall-btn");
+  let task = target.parentElement;
+
+  // check task
+  if (target.classList.contains("check")) {
+    task.classList.toggle("completed");
+    task.classList.toggle("active");
+    if (completedBtn.matches(".clicked")) {
+      task.style.display = "none";
+    }
+    if (activeBtn.matches(".clicked")) {
+      task.style.display = "none";
+    }
+  }
+
+  // delete task
+  if (target.classList.contains("delete")) {
+    task.remove();
+  }
+
+  if (target.classList.contains("clear-task")) {
     taskCompleted_el.forEach((task) => {
       task.remove();
     });
   }
+  if (target.matches(".checkall-btn.unclicked")) {
+    // check all btn
+    task_el.forEach((task) => {
+      task.classList.remove("active");
+      task.classList.add("completed");
+      checkallBtn.style.color = "#797777";
+      checkallBtn.classList.remove("unclicked");
+      checkallBtn.classList.add("clicked");
+    });
+  } else if (target.matches(".checkall-btn.clicked")) {
+    task_el.forEach((task) => {
+      task.classList.remove("completed");
+      task.classList.add("active");
+      checkallBtn.style.color = "#e6e6e6";
+      checkallBtn.classList.remove("clicked");
+      checkallBtn.classList.add("unclicked");
+    });
+  }
+
   displayItem();
+  saveToLocalStorage();
 }
 
-input.addEventListener("keydown", createTask);
-taskSection_el.addEventListener("dblclick", updateTask);
-document.addEventListener("click", deleteTask);
-document.addEventListener("click", filterSection_logic);
-arrowButton_el.addEventListener("click", arrowBtn_logic);
+function displayDeletebtn(e) {
+  const target = e.target;
+  switch (e.type) {
+    case "mouseover":
+      if (target.classList.contains("task") || target.closest(".task")) {
+        const deleteBtn = target.closest(".task").children[2];
+        deleteBtn.style.visibility = "visible";
+        saveToLocalStorage();
+      }
+      break;
+    case "mouseout":
+      if (target.classList.contains("task") || target.closest(".task")) {
+        const deleteBtn = target.closest(".task").children[2];
+        deleteBtn.style.visibility = "hidden";
+        saveToLocalStorage();
+      }
+      break;
+  }
+}
 
 function displayItem() {
   const task_el = document.querySelectorAll(".task");
-  const taskActive_el = document.querySelectorAll(".task.active");
   const taskCompleted_el = document.querySelectorAll(".task.completed");
+  const taskActive_el = document.querySelectorAll(".task.active");
+  const filterSection_el = document.querySelector(".filter-section");
+  const clearbtn = document.querySelector(".clear-task");
   const itemCounter_el = document.querySelector(".items-left");
-  const clearButton_el = document.querySelector(".clear-task");
-  // display arrow button and filter section
+  const checkallBtn = document.querySelector(".checkall-btn");
+
+  // Display Clear Button
+  if (taskCompleted_el.length > 0) {
+    clearbtn.classList.remove("hidden");
+  } else {
+    clearbtn.classList.add("hidden");
+  }
+
+  // Display Filter Section
   if (task_el.length > 0) {
-    arrowButton_el.classList.remove("hidden");
     filterSection_el.classList.remove("hidden");
   } else {
-    arrowButton_el.classList.add("hidden");
     filterSection_el.classList.add("hidden");
   }
-  // display item counter
-  itemCounter_el.textContent = `${taskActive_el.length} item left`;
 
-  // display clear button
-  if (taskCompleted_el.length > 0) {
-    clearButton_el.classList.remove("hidden");
-  }
+  // Display Item Counter
+  const taskLength = task_el.length - taskCompleted_el.length;
+  itemCounter_el.innerHTML = `${taskLength} items left`;
 
-  // display arrow button or hide
+  // Display CheckAll Button
   if (taskActive_el.length == 0 && taskCompleted_el.length > 0) {
-    arrowButton_el.classList.remove("unclicked");
-    arrowButton_el.classList.add("clicked");
-    arrowButton_el.src = "images/arrow-clicked.png";
-  } else if (taskActive_el.length > 0) {
-    arrowButton_el.classList.remove("clicked");
-    arrowButton_el.classList.add("unclicked");
-    arrowButton_el.src = "images/arrow.png";
+    checkallBtn.style.color = "#797777";
+    checkallBtn.classList.remove("unclicked");
+    checkallBtn.classList.add("clicked");
+  } else if (taskActive_el.length > 0 || task_el.length == 0) {
+    checkallBtn.style.color = "#e6e6e6";
+    checkallBtn.classList.remove("clicked");
+    checkallBtn.classList.add("unclicked");
   }
 }
 
-function arrowBtn_logic(e) {
-  const target = e.target;
-  const taskAll_el = document.querySelectorAll(".task");
+function saveToLocalStorage() {
+  localStorage.setItem("data", taskSection_el.innerHTML);
+}
 
-  if (target.matches(".unclicked")) {
-    taskAll_el.forEach((task) => {
-      const checkBtn = task.children[0];
-      task.classList.replace("active", "completed");
-      checkBtn.classList.replace("unchecked", "checked");
-      checkBtn.src = "images/checked-btn.png";
-    });
-    arrowButton_el.classList.replace("unclicked", "clicked");
-  } else if (target.matches(".clicked")) {
-    taskAll_el.forEach((task) => {
-      const checkBtn = task.children[0];
-      task.classList.replace("completed", "active");
-      checkBtn.classList.replace("checked", "unchecked");
-      checkBtn.src = "images/unchecked-btn.png";
-    });
-    arrowButton_el.classList.replace("clicked", "unclicked");
-  }
+function getFromLocalStorage() {
+  taskSection_el.innerHTML = localStorage.getItem("data");
   displayItem();
-}
-
-function filterSection_logic(e) {
-  const target = e.target;
-  const task_el = document.querySelectorAll(".task");
-  if (target.matches(".active-button")) {
-    task_el.forEach((task) => {
-      allBtn.classList.remove("active");
-      completedBtn.classList.remove("active");
-      activeBtn.classList.add("active");
-      if (task.matches(".completed")) {
-        task.classList.add("hidden");
-      } else {
-        task.classList.remove("hidden");
-      }
-    });
-  }
-  if (target.matches(".completed-button")) {
-    task_el.forEach((task) => {
-      allBtn.classList.remove("active");
-      activeBtn.classList.remove("active");
-      completedBtn.classList.add("active");
-      if (task.matches(".active")) {
-        task.classList.add("hidden");
-      } else {
-        task.classList.remove("hidden");
-      }
-    });
-  }
-  if (target.matches(".all-button")) {
-    task_el.forEach((task) => {
-      activeBtn.classList.remove("active");
-      completedBtn.classList.remove("active");
-      allBtn.classList.add("active");
-      task.classList.remove("hidden");
-    });
-  }
 }
